4a617f7d0c7416f0ab7da26e76d2876192a308a7
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-06 17:29:13 -0400

    module 3 calculator assignment (switch cases)

---
+++ Calculator/Module_3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Module_3
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double firstOperand;
+            double secondOperand;
+            double result;
+            string oprt;
+
+            Console.Write("Enter first operand: ");
+            firstOperand = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second operand: ");
+            secondOperand = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+ - / *): ");
+            oprt = Console.ReadLine();
+
+            switch (oprt)
+            {
+                case "+":
+                    Console.WriteLine("Result: {0} + {1} = {2}", firstOperand, secondOperand, result = firstOperand + secondOperand);
+                    break;
+                case "-":
+                    Console.WriteLine("Result: {0} - {1} = {2}", firstOperand, secondOperand, result = firstOperand - secondOperand);
+                    break;
+                case "*":
+                    Console.WriteLine("Result: {0} * {1} = {2}", firstOperand, secondOperand, result = firstOperand * secondOperand);
+                    break;
+                case "/":
+                    Console.WriteLine("Result: {0} / {1} = {2}", firstOperand, secondOperand, result = firstOperand / secondOperand);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator. No calcuations made.");
+                    break;
+            }
+        }
+    }
+}
