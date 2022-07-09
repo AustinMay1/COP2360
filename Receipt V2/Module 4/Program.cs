5fa2e03bc9ea0e919c001f556e1546f3346b4ef2
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-09 14:49:12 -0400

    Receipt V2 (do while)

---
+++ Receipt V2/Module 4/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Module_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal count = 1;
+            decimal price;
+            decimal subtotal = 0;
+            decimal tax; ;
+            decimal total;
+
+            Console.WriteLine("Receipt V2 - COP2360 - Austin May");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Item {0}", count + " " + "Enter price: $");
+                count++;
+                price = Convert.ToDecimal(Console.ReadLine());
+                subtotal += price;
+                tax = subtotal * 0.06M;
+                total = subtotal + tax + 1;
+
+            }
+            while (price != -1);
+
+            Console.WriteLine();
+            Console.WriteLine("=================================");
+            Console.WriteLine("Number of items: {0}", count - 2);
+            Console.WriteLine("Subtotal: ${0}", subtotal + 1);
+            Console.WriteLine("Tax: ${0}", tax + 0.1M);
+            Console.WriteLine("Total: ${0}", total + 0.1M);
+
+            
+           
+        }
+    }
+}
