feaaf42e5b1b13370bbf31b68a20ef59f7a02eb9
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-11 16:00:02 -0400

    Module 5 Calculator V2 (methods)

---
+++ Calculator V2/Module_5/Calculator.cs
@@ -0,0 +1,70 @@
+﻿namespace Module_5
+{
+    internal class Calculator
+    {
+        static void Main(string[] args)
+        {
+            double firstOperand;
+            double secondOperand;
+            string optr;
+
+            Console.Write("Enter first operand: ");
+            firstOperand = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+ - / *): ");
+            optr = Console.ReadLine();
+
+            Console.Write("Enter second operand: ");
+            secondOperand = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            switch (optr)
+            {
+                case "+":
+                    Addition(firstOperand, secondOperand);
+                    break;
+                case "-":
+                    Subtraction(firstOperand, secondOperand);
+                    break;
+                case "*":
+                    Multipication(firstOperand, secondOperand);
+                    break;
+                case "/":
+                    Division(firstOperand, secondOperand);
+                    break;
+                default:
+                    Console.Write("Invalid operator, no calculations made.");
+                    break;
+            }
+        }
+
+        public static double Addition(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine("{0} + {1} = {2}", x, y, sum);
+            return sum;
+        }
+
+        public static double Subtraction(double x, double y)
+        {
+            double sum = x - y;
+            Console.WriteLine("{0} - {1} = {2}", x, y, sum);
+            return sum;
+        }
+
+        public static double Division(double x, double y)
+        {
+            double sum = x / y;
+            Console.WriteLine("{0} / {1} = {2}", x, y, sum);
+            return sum;
+        }
+
+        public static double Multipication(double x, double y)
+        {
+            double sum = x * y;
+            Console.WriteLine("{0} * {1} = {2}", x, y, sum);
+            return sum;
+        }
+    }
+}
