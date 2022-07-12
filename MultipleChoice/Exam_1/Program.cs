a3673e1b416b302fe78cb926ebf0103e3bc108c3
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-12 17:30:20 -0400

    Exam_1 - Multiple Choice (while, if, switch)

---
+++ MultipleChoice/Exam_1/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Exam_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempt = 1;
+            int score = 0;
+            string answerOne;
+            string answerTwo;
+            string userChoice;
+
+            while (attempt <= 2)
+            {
+                Console.Write("\nAttempt {0}", attempt);
+                Console.Write("\n1. Where is the capital of the state of Florida?\n A. Orlando\n B. Tallahassee\n C. Miami\n D. Tampa\n Answer: ");
+                answerOne = Console.ReadLine().ToUpper();
+
+                switch (answerOne)
+                {
+                    case "A":
+                        break;
+                    case "B":
+                        score += 50;
+                        break;
+                    case "C":
+                        break;
+                    case "D":
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Write("\n2. Where is Walt Disney World Park located in Florida?\n A. Orlando\n B. Tallahassee\n C. Miami\n D. Tampa\n Answer: ");
+                answerTwo = Console.ReadLine().ToUpper();
+
+                switch (answerTwo)
+                {
+                    case "A":
+                        score += 50;
+                        break;
+                    case "B":
+                        break;
+                    case "C":
+                        break;
+                    case "D":
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("\nScore: {0}", score);
+
+                if (score < 100)
+                {
+                    Console.Write("\nWould you like to try again? (y / n): ");
+                    userChoice = Console.ReadLine().ToUpper();
+
+                    if (userChoice == "Y")
+                    {
+                        score = 0;
+                        attempt++;
+
+                    }
+                    else if (userChoice == "N")
+                    {
+                        break;
+                    }
+                }
+                else if (score == 100)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
