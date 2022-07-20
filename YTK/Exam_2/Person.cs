680ed60bb9190df079f5340e4a4c9d26de961eca
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-20 15:49:01 -0400

    Exam 2 - Retirement Calculator (classes, properties)

---
+++ YTK/Exam_2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_2
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        private int yearsToWork;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int YearsToWork
+        {
+            get { return yearsToWork; }
+            set { yearsToWork = value; }
+        }
+
+        public int YTK()
+        {
+            YearsToWork = 65 - Age;
+            return YearsToWork;
+        }
+    }
+}
---
+++ YTK/Exam_2/PersonRun.cs
@@ -0,0 +1,20 @@
+﻿namespace Exam_2
+{
+    internal class PersonRun
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            Console.Write("Enter your name: ");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            person.Age = Convert.ToInt16(Console.ReadLine());
+
+            person.YTK();
+
+            Console.WriteLine("{0}, you have {1} years left to work until you can retire.", person.Name, person.YearsToWork);
+        }
+    }
+}
