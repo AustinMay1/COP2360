b7da5cfe783926aada6665692a11d7b2fa121ecd
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-23 22:13:57 -0400

    Module_9 - Students (classes cont.)

---
+++ Students/Module_9/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Module_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.FirstName = "John";
+            s1.LastName = "Smith";
+            s1.StudentID = 2560;
+
+            Student s2 = new Student("Peter");
+
+            Student s3 = new Student("Morgan", "Simmons");
+
+            Student s4 = new Student("James", "Walters");
+
+            Student s5 = new Student("Linda", "Scott", 1005);
+
+            Console.WriteLine();
+            Console.WriteLine("Total students: {0}", Student.Count);
+
+            Console.WriteLine("\n======Student List======");
+            Console.WriteLine("Student: " + s1.FirstName + " " + s1.LastName + " " + "ID: {0}", s1.StudentID);
+            Console.WriteLine("Student: " + s2.FirstName + " " + s2.LastName + " " + "ID: {0}", s2.StudentID);
+            Console.WriteLine("Student: " + s3.FirstName + " " + s3.LastName + " " + "ID: {0}", s3.StudentID);
+            Console.WriteLine("Student: " + s4.FirstName + " " + s4.LastName + " " + "ID: {0}", s4.StudentID);
+            Console.WriteLine("Student: " + s5.FirstName + " " + s5.LastName + " " + "ID: {0}", s5.StudentID);
+        }
+    }
+}
---
+++ Students/Module_9/Student.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_9
+{
+    internal class Student
+    {
+        public static int Count = 0;
+        private static readonly Random rnd = new Random();
+        private string firstName;
+        private string lastName;
+        private int sID;
+
+        public Student(string first, string last, int id)
+        {
+            firstName = first;
+            lastName = last;
+            sID = id;
+            Count++;
+        }
+
+        public Student(string first = "", string last = "")
+        {
+            firstName = first;
+            lastName = last;
+            sID = rnd.Next(1000, 9999);
+            Count++;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+
+        public int StudentID
+        {
+            get { return this.sID; }
+            set { this.sID = value; }
+        }
+    }
+}
