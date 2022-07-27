d62355342e854f8a5e05f466e79f8ded09f3e43b
Not-Ur-Hero <austin.may@protonmail.com>
2022-07-26 22:22:49 -0400

    module_10 - (class with inheritance)

---
+++ DSC/Module_10/Campus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_10
+{
+    internal class Campus:DSC
+    {
+
+        private string campusName;
+
+        public string CampusName
+        {
+            get { return campusName; }
+            set { campusName = value; }
+        }
+
+        public Campus(string cName)
+        {
+            campusName = cName;
+        }
+
+        public override string ShowAddress()
+        {
+            return "1770 Williamson Blvd., Daytona Beach, Florida 32117";
+        }
+
+        public string Departments()
+        {
+            return "Computer Scinece Department, Emergency Care Department, Police Academy";
+        }
+
+        public override string ToString()
+        {
+            return getSchoolName() + "\nis located at " + ShowAddress() + "\nit has " + Departments();
+        }
+    }
+}
---
+++ DSC/Module_10/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Module_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Campus atc = new Campus("Advanced Technology College");
+            
+            Console.WriteLine(atc.ToString());
+        }
+    }
+}
